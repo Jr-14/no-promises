02ca63e4efbb34fd1e0060a1a9e91269b253d843
John Emerson Requizo <jr.jemerson14@gmail.com>
2025-08-13 20:30:52 +1000

    Add some initial structs

--- src/thread_futures.c
+++ src/thread_futures.c
@@ -6,6 +6,23 @@ typedef struct cont {
     void *arg; struct cont *next;
 } cont_t;
 
+typedef struct {
+    pthread_mutex_t mtx;
+    pthread_cond_t  cv;
+    int             ready;
+    int             has_err;
+    void            *val;
+    cont_t          *conts;
+} shared_state_t;
+
+typedef struct {
+    shared_state_t *s;
+} future_t;
+
+typedef struct {
+    shared_state_t *s;
+} promise_t;
+
 
 int main() {
     printf("hello, world!\n");
