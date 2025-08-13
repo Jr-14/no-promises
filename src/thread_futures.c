49ed8ba5dc204fa50aa2a94338500515dfce8c35
John Emerson Requizo <jr.jemerson14@gmail.com>
2025-08-13 20:25:29 +1000

    Remove binary

--- src/thread_futures.c
+++ src/thread_futures.c
@@ -1,6 +1,12 @@
 #include <pthread.h>
 #include <stdio.h>
 
+typedef struct cont {
+    void (*fn)(void *arg, void *val);
+    void *arg; struct cont *next;
+} cont_t;
+
+
 int main() {
     printf("hello, world!\n");
 
